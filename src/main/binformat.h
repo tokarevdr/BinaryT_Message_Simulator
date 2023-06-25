98bc7a73b0f0e3b6cbbb9337e9b575c575509a12
pavelko294 <100138840+pavelko294@users.noreply.github.com>
2023-06-25 14:59:02 +0300

    Update binformat.h
    Добавил Формат BIN32

--- src/main/binformat.h
+++ src/main/binformat.h
@@ -17,3 +17,20 @@ public:
 private:
     quint16 value_;
 };
+
+/* ............ */ 
+
+class bin32
+{
+public:
+
+    bin32(quint32 value = 0);
+
+    void SetBit32(quint8 bit, bool value);
+    bool GetBit32(quint8 bit) const;
+    quint32 GetBits32(quint8 bit_begin, quint8 bit_end) const;
+    void SetValue32(quint32 buffer);
+
+private:
+    quint32 value_;
+};
