4a0bca496150af8b6ecb1811e08ea298363747a0
pavelko294 <100138840+pavelko294@users.noreply.github.com>
2023-06-25 15:28:34 +0300

    Update and rename binformat.h to bin16.h

--- src/main/bin16.h
+++ src/main/bin16.h
@@ -17,20 +17,3 @@ public:
 private:
     quint16 value_;
 };
-
-/* ............ */ 
-
-class bin32
-{
-public:
-
-    bin32(quint32 value = 0);
-
-    void SetBit32(quint8 bit, bool value);
-    bool GetBit32(quint8 bit) const;
-    quint32 GetBits32(quint8 bit_begin, quint8 bit_end) const;
-    void SetValue32(quint32 buffer);
-
-private:
-    quint32 value_;
-};
