6aaf5ad51f8f38e74e0f07aa3fd161e365634688
pavelko294 <100138840+pavelko294@users.noreply.github.com>
2023-06-25 14:44:45 +0300

    Create binformat.h

---
+++ binformat.h
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+class bin16
+{
+
+public:
+
+    bin16(quint16 value = 0);
+
+    void SetBit(quint8 bit, bool value);
+    bool GetBit(quint8 bit) const;
+    quint16 GetBits(quint8 bit_begin, quint8 bit_end) const;
+    void SetValue(quint16 buffer);
+
+private:
+    quint16 value_;
+};
