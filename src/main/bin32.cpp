b2778eb62d7be7f265db38060f95a5822c033129
pavelko294 <100138840+pavelko294@users.noreply.github.com>
2023-06-25 16:01:06 +0300

    Update bin32.cpp

--- src/main/bin32.cpp
+++ src/main/bin32.cpp
@@ -1,3 +1,5 @@
+#include <QDebug>
+
 #include "bin32.h"
 
 bin32::bin32(quint32 value)
@@ -5,29 +7,37 @@ bin32::bin32(quint32 value)
     value_ = value;
 }
 
-void bin32::SetBit32(quint8 bit, bool value)
+void bin32::SetBit(quint8 bit, bool value)
 {
+    if (bit >= 0 && bit <= 31){
     //устанавливаем бит по адресу
     if (value != false)
         value_ |= (1 << bit);
-    else {
-        value_ &= ~(1 << bit);
+    else 
+        value_ &= ~(1 << bit);    
     }
+    else qDebug()<< "Inserted bit is out of range";
 }
 
-void bin32::SetValue32(quint32 buffer)
+void bin32::SetValue(quint8 buffer)
 {
+    if(buffer >= 0 && buffer <= 4294967295)
     //устанавливаем значение
     value_ |= buffer;
+    else qDebug() << "Inserted value is out of range";
+
 }
 
-bool bin32::GetBit32(quint8 bit) const
+bool bin32::GetBit(quint8 bit) const
 {
+    if (bit>=0 && bit <=31)
     return value_ & (1 << bit);
+    else return "Inserted bit is out of range";
 }
 
-quint32 bin32::GetBits32(quint8 bit_begin, quint8 bit_end) const
+quint32 bin32::GetBits(quint8 bit_begin, quint8 bit_end) const
 {
+    if (bit_begin <=31 && bit_begin >=0 && bit_end >=0 && bit_end <=31 && bit_end > bit_begin){
     quint8 length = bit_end - bit_begin;
     quint16 mask = 1;
     for(int i = 1; i<= length; i++){
@@ -35,5 +45,6 @@ quint32 bin32::GetBits32(quint8 bit_begin, quint8 bit_end) const
         mask++;
     }
     return (value_ >> bit_begin) & mask;
-
+}
+    else return "Inserted bit is out of range";
 }
