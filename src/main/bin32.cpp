929467a78d788b353ff97208b566597802847f62
pavelko294 <100138840+pavelko294@users.noreply.github.com>
2023-06-25 17:11:02 +0300

    Обновлен текст вывода ошибок, проверка на не вхождение в диапазон

--- src/main/bin32.cpp
+++ src/main/bin32.cpp
@@ -9,35 +9,51 @@ bin32::bin32(quint32 value)
 
 void bin32::SetBit(quint8 bit, bool value)
 {
-    if (bit >= 0 && bit <= 31){
+    if (bit < 0 && bit > 31)
+    {
+        qDebug()<< "In class BIN32, in method SetBit(), error: Inserted bit is out of range";
+        return;
+    }
+    else
+    {
     //устанавливаем бит по адресу
     if (value != false)
         value_ |= (1 << bit);
-    else 
-        value_ &= ~(1 << bit);    
+    else
+        value_ &= ~(1 << bit);
     }
-    else qDebug()<< "Inserted bit is out of range";
 }
 
 void bin32::SetValue(quint32 buffer)
 {
-    if(buffer >= 0 && buffer <= 4294967295)
     //устанавливаем значение
     value_ |= buffer;
-    else qDebug() << "Inserted value is out of range";
-
 }
 
 bool bin32::GetBit(quint8 bit) const
 {
-    if (bit>=0 && bit <=31)
-    return value_ & (1 << bit);
-    else qDebug() << "Inserted bit is out of range";
+    if (bit<0 && bit > 31)
+    {
+     qDebug() << "In class BIN32, in method GetBit(), error: Inserted bit is out of range";
+     return false;
+    }
+    else return value_ & (1 << bit);;
 }
 
 quint32 bin32::GetBits(quint8 bit_begin, quint8 bit_end) const
 {
-    if (bit_begin <=31 && bit_begin >=0 && bit_end >=0 && bit_end <=31 && bit_end > bit_begin){
+    if (bit_begin > 31 && bit_begin < 0 && bit_end < 0 && bit_end > 31)
+    {
+        qDebug() << "In class BIN32, in method GetBits(), error: Inserted bit is out of range";
+        return 0;
+    }
+    else if (bit_end < bit_begin)
+    {
+        qDebug() << "In class BIN32, in method GetBits(), error: The bit order is entered incorrectly";
+        return 0;
+    }
+    else
+    {
     quint8 length = bit_end - bit_begin;
     quint16 mask = 1;
     for(int i = 1; i<= length; i++){
@@ -45,6 +61,5 @@ quint32 bin32::GetBits(quint8 bit_begin, quint8 bit_end) const
         mask++;
     }
     return (value_ >> bit_begin) & mask;
-}
-    else qDebug() << "Inserted bit is out of range";
+    }
 }
