f84cb403217eff2437d68afdbe9ce516b1e5cfea
pavelko294 <100138840+pavelko294@users.noreply.github.com>
2023-06-27 20:34:49 +0300

    Update bin16.cpp

--- src/main/bin16.cpp
+++ src/main/bin16.cpp
@@ -1,4 +1,4 @@
-#include <QDebug>
+#include <QtGlobal>
 
 #include "bin16.h"
  
@@ -9,19 +9,9 @@ bin16::bin16(quint16 value)
 
 void bin16::SetBit(quint8 bit, bool value)
 {
-    if (bit < 0 && bit > 15)
-    {
-        qDebug()<< "bin16:SetBit(): Inserted bit is out of range";
-        return;
-    }
-    else
-    {
+    Q_ASSERT(bit <= 15);
     //устанавливаем бит по адресу
-    if (value != false)
-        value_ |= (1 << bit);
-    else
-        value_ &= ~(1 << bit);
-    }
+    value ? value_ |= (1 << bit) : value_ &= ~(1 << bit);
 }
 
 void bin16::SetValue(quint16 buffer)
@@ -32,35 +22,20 @@ void bin16::SetValue(quint16 buffer)
 
 bool bin16::GetBit(quint8 bit) const
 {
-    if (bit<0 && bit > 15)
-    {
-     qDebug() << "bin16:GetBit(): Inserted bit is out of range";
-     return false;
-    }
-    else return value_ & (1 << bit);;
+    Q_ASSERT(bit <= 15);
+    return value_ & (1 << bit);
 }
 
 quint16 bin16::GetBits(quint8 bit_begin, quint8 bit_end) const
 {
-    if (bit_begin > 15 && bit_begin < 0 && bit_end < 0 && bit_end > 15)
-    {
-        qDebug() << "bin16:GetBits(): Inserted bit is out of range";
-        return 0;
-    }
-    else if (bit_end < bit_begin)
-    {
-        qDebug() << "bin16:GetBits(): start bit > end bit";
-        return 0;
-    }
-    else
-    {
+    Q_ASSERT(bit_end <= 15 && bit_end > bit_begin);
+
     quint8 length = bit_end - bit_begin;
     quint16 mask = 1;
-    for(int i = 1; i<= length; i++){
+    for(int i = 1; i<= length; i++)
+    {
         mask <<= 1;
         mask++;
     }
     return (value_ >> bit_begin) & mask;
-    }
-
 }
