053343e1f761cc192a31e8965d5b80280b7a03eb
Dmitry Tokarev <verakotishe@gmail.com>
2023-07-27 15:22:24 +0300

    Реализовал новый интерфейс сообщений №9

--- src/main/messages/coordinatespeedtimemessage.cpp
+++ src/main/messages/coordinatespeedtimemessage.cpp
@@ -1,7 +1,127 @@
 #include "coordinatespeedtimemessage.h"
 
 CoordinateSpeedTimeMessage::CoordinateSpeedTimeMessage() :
-    AbstractMessage(1, 1) // TODO
+    AbstractMessage(9, 25)
 {
 
+}
+
+quint16 CoordinateSpeedTimeMessage::checkSum() const
+{
+    return 0;
+}
+
+QByteArray CoordinateSpeedTimeMessage::formPackage() const
+{
+    return QByteArray();
+}
+
+quint8 CoordinateSpeedTimeMessage::rowCount() const
+{
+    return 11;
+}
+
+QString CoordinateSpeedTimeMessage::fieldName(quint8 row) const
+{
+    if (row > 10) return QString();
+
+    switch (row)
+    {
+    case 0: return "Слово готовности";
+    case 1: return "Признаки системы координат";
+    case 2: return "Номер эллипсоида";
+    case 3: return "Дата";
+    case 4: return "Время";
+    case 5: return "Широта (координата X)";
+    case 6: return "Долгота (координата Y)";
+    case 7: return "Высота (координата Z)";
+    case 8: return "Составляющая скорости\n"
+                   "по широте (X)";
+    case 9: return "Составляющая скорости\n"
+                   "по долготе (Y)";
+    case 10: return "Составляющая скорости\n"
+                    "по высоте (Z)";
+    default: return QString();
+    }
+}
+
+AbstractMessage::FieldType CoordinateSpeedTimeMessage::fieldType(quint8 row) const
+{
+    if (row > 10) return AbstractMessage::FieldType::Invalid;
+
+    switch (row)
+    {
+    case 0: return AbstractMessage::FieldType::Bin16;
+    case 1: return AbstractMessage::FieldType::Bin16;
+    case 2: return AbstractMessage::FieldType::UInt16;
+    case 3: return AbstractMessage::FieldType::Bin32;
+    case 4: return AbstractMessage::FieldType::Bin32;
+    case 5: return AbstractMessage::FieldType::Double;
+    case 6: return AbstractMessage::FieldType::Double;
+    case 7: return AbstractMessage::FieldType::Double;
+    case 8: return AbstractMessage::FieldType::Float;
+    case 9: return AbstractMessage::FieldType::Float;
+    case 10: return AbstractMessage::FieldType::Float;
+    default: return AbstractMessage::FieldType::Invalid;
+    }
+}
+
+bool CoordinateSpeedTimeMessage::setField(quint8 row, QVariant value)
+{
+    if (row > 10) return false;
+
+    switch (row) {
+    case 0:
+        return true;
+    case 1:
+        return true;
+    case 2:
+        ellipsoid_number_ = value.toInt();
+        return true;
+    case 3:
+        return true;
+    case 4:
+        return true;
+    case 5:
+         latitude_ = value.toDouble();
+        return true;
+    case 6:
+         longitude_ = value.toDouble();
+        return true;
+    case 7:
+         altitude_ = value.toDouble();
+        return true;
+    case 8:
+         speed_latitude_component_ = value.toFloat();
+        return true;
+    case 9:
+         speed_longitude_component_ = value.toFloat();
+        return true;
+    case 10:
+         speed_altitude_component_ = value.toFloat();
+        return true;
+    default:
+        return false;
+    }
+}
+
+QVariant CoordinateSpeedTimeMessage::field(quint8 row) const
+{
+    if (row > 10) return QVariant();
+
+    switch (row)
+    {
+    case 0: return QVariant();
+    case 1: return QVariant();
+    case 2: return ellipsoid_number_;
+    case 3: return QVariant();
+    case 4: return QVariant();
+    case 5: return latitude_;
+    case 6: return longitude_;
+    case 7: return altitude_;
+    case 8: return speed_latitude_component_;
+    case 9: return speed_longitude_component_;
+    case 10: return speed_altitude_component_;
+    default: return QVariant();
+    }
 }
--- src/main/messages/coordinatespeedtimemessage.h
+++ src/main/messages/coordinatespeedtimemessage.h
@@ -1,28 +1,39 @@
 #ifndef COORDINATESPEEDTIMEMESSAGE_H
 #define COORDINATESPEEDTIMEMESSAGE_H
 
-#include <abstractmessage.h>
+#include "abstractmessage.h"
+
+#include <QVariant>
 
 #include "utilities/bin16.h"
 #include "utilities/bin32.h"
 
-    class CoordinateSpeedTimeMessage : public AbstractMessage
-    {
-    private:
-        bin16 readness_word_;
-        bin16 coordinates_systems_signs_;
-        quint16 ellipsoid_number_;
-        bin32 date_;
-        bin32 time_;
-        double latitude_;
-        double longitude_;
-        double altitude_;
-        float speed_latitude_component_;
-        float speed_longitude_component_;
-        float speed_altitude_component_;
-    public:
-        CoordinateSpeedTimeMessage();
-    };
+class CoordinateSpeedTimeMessage : public AbstractMessage
+{
+public:
+    CoordinateSpeedTimeMessage();
 
+    virtual quint16 checkSum() const override;
+    virtual QByteArray formPackage() const override;
+
+    virtual quint8 rowCount() const override;
+    virtual QString fieldName(quint8 row) const override;
+    virtual FieldType fieldType(quint8 row) const override;
+    virtual bool setField(quint8 row, QVariant value) override;
+    virtual QVariant field(quint8 row) const override;
+
+private:
+    bin16 readness_word_;
+    bin16 coordinates_systems_signs_;
+    quint16 ellipsoid_number_ = 0;
+    bin32 date_;
+    bin32 time_;
+    double latitude_ = 0.;
+    double longitude_ = 0.;
+    double altitude_ = 0.;
+    float speed_latitude_component_ = 0.;
+    float speed_longitude_component_ = 0.;
+    float speed_altitude_component_ = 0.;
+};
 
 #endif // COORDINATESPEEDTIMEMESSAGE_H
