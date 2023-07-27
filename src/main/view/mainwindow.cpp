10a45714e8e3b7faa5de27be3c2955d846df500e
Dmitry Tokarev <verakotishe@gmail.com>
2023-07-27 15:29:58 +0300

    Реализовал функционал comboBox'а Реализовал растягивание таблицы Изменил модель Добавил новый делегат

--- src/main/view/mainwindow.cpp
+++ src/main/view/mainwindow.cpp
@@ -4,9 +4,10 @@
 #include <QModelIndex>
 #include <QLineEdit>
 
-#include "view/double_message_field_table_widget_delegate.h"
-#include "view/float_message_field_table_widget_delegate.h"
-#include "view/uint16_message_field_table_widget_delegate.h"
+#include "double_message_field_delegate.h"
+#include "float_message_field_delegate.h"
+#include "uint16_message_field_delegate.h"
+#include "uint32_message_field_delegate.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -14,12 +15,16 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    _messageFieldsModel = new MessageFieldsModel();
+    messageModel_ = new MessageModel();
 
-    ui->tableView_messageFields->setModel(_messageFieldsModel);
-    ui->tableView_messageFields->horizontalHeader()->setVisible(true);
+    ui->comboBox_messageNames->addItems(messageModel_->messageNames());
+    ui->comboBox_messageNames->setCurrentIndex(-1);
 
-    setTableViewFields(_messageFieldsModel->getMessageFieldsType());
+    ui->tableView_messageFields->setModel(messageModel_);
+    ui->tableView_messageFields->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView_messageFields->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    setTableViewFields(messageModel_->fieldTypes());
 }
 
 MainWindow::~MainWindow()
@@ -27,31 +32,38 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::setTableViewFields(QList<IMessageFields::FieldType> fieldsType)
+void MainWindow::setTableViewFields(QList<AbstractMessage::FieldType> fieldsType)
 {
     int i = 0;
-    foreach(IMessageFields::FieldType fieldType, fieldsType)
+    foreach(AbstractMessage::FieldType fieldType, fieldsType)
     {
 
         switch(fieldType)
         {
-        case IMessageFields::FieldType::Double:
+        case AbstractMessage::FieldType::Double:
         {
-            DoubleMessageFieldTableWidgetDelegate *delegate = new DoubleMessageFieldTableWidgetDelegate();
+            DoubleMessageFieldDelegate *delegate = new DoubleMessageFieldDelegate();
             ui->tableView_messageFields->setItemDelegateForRow(i, delegate);
             break;
         }
 
-        case IMessageFields::FieldType::Float:
+        case AbstractMessage::FieldType::Float:
         {
-            FloatMessageFieldTableWidgetDelegate *delegate = new FloatMessageFieldTableWidgetDelegate();
+            FloatMessageFieldDelegate *delegate = new FloatMessageFieldDelegate();
             ui->tableView_messageFields->setItemDelegateForRow(i, delegate);
             break;
         }
 
-        case IMessageFields::FieldType::Uint16:
+        case AbstractMessage::FieldType::UInt16:
         {
-            UInt16MessageFieldTableWidgetDelegate *delegate = new UInt16MessageFieldTableWidgetDelegate();
+            UInt16MessageFieldDelegate *delegate = new UInt16MessageFieldDelegate();
+            ui->tableView_messageFields->setItemDelegateForRow(i, delegate);
+            break;
+        }
+
+        case AbstractMessage::FieldType::UInt32:
+        {
+            UInt32MessageFieldDelegate *delegate = new UInt32MessageFieldDelegate();
             ui->tableView_messageFields->setItemDelegateForRow(i, delegate);
             break;
         }
@@ -66,5 +78,13 @@ void MainWindow::setTableViewFields(QList<IMessageFields::FieldType> fieldsType)
 
         ++i;
     }
+}
+
+
+void MainWindow::on_comboBox_messageNames_currentTextChanged(const QString &arg1)
+{
+    messageModel_->setCurrentMessage(arg1);
+
+    setTableViewFields(messageModel_->fieldTypes());
 }
 
